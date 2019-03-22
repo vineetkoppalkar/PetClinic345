9efc2e808ae6c6336364f1d31f5f6416ed08b46a
dfeokti <d_feokti@live.concordia.ca>
2019-03-21 23:29:07 -0400

    Select statement from in-memory database example

--- src/main/java/org/springframework/samples/petclinic/migration/ForkliftRunner.java
+++ src/main/java/org/springframework/samples/petclinic/migration/ForkliftRunner.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.migration;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.sql.*;
 
 @Component
 public class ForkliftRunner {
@@ -12,5 +13,22 @@ public class ForkliftRunner {
         Thread migration = new Thread(new Forklift());
         migration.setPriority(Thread.MIN_PRIORITY);
         migration.start();
+
+        String url = "jdbc:hsqldb:mem:petclinic;readonly=true";
+        Connection c;
+        try {
+            c = DriverManager.getConnection(url, "sa", "");
+            Statement st = c.createStatement();
+            String query = "select * from owners";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                String firstName = rs.getString("first_name");
+                System.out.println(firstName);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
